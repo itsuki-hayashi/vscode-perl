6581cd0a855dfd47f272c8a7b2de94b7752f612f
edsgerlin <edsgerlin@icloud.com>
2019-01-21 03:42:55 +0900

    Refine code.

--- src/format.ts
+++ src/format.ts
@@ -19,47 +19,34 @@ export class PerlFormattingProvider implements DocumentRangeFormattingEditProvid
             }
 
             const config = workspace.getConfiguration("perl");
-
             const executable = config.get("perltidy", "perltidy");
             const args = config.get("perltidyArgs", ["-q"]);
-
             const text = document.getText(range);
             const child = spawn(executable, args);
+
             child.stdin.write(text);
             child.stdin.end();
 
-            let stdout = "";
-            child.stdout.on("data", (out: Buffer) => {
-                stdout += out.toString();
+            const stdoutChunks: string[]  = [];
+            child.stdout.on("data", (chunk: Buffer) => {
+                stdoutChunks.push(chunk.toString());
             });
 
-            let stderr = "";
-            child.stderr.on("data", (out: Buffer) => {
-                stderr += out.toString();
+            const stderrChunks: string[] = [];
+            child.stderr.on("data", (chunk: Buffer) => {
+                stderrChunks.push(chunk.toString());
             });
 
-            let error: Error;
-            child.on("error", (err: Error) => {
-                error = err;
+            child.on("error", (error: Error) => {
+                stderrChunks.push(error.message);
             });
 
             child.on("close", (code) => {
-                let message = "";
-
-                if (error) {
-                    message = error.message;
-                } else if (stderr) {
-                    message = stderr;
-                } else if (code !== 0) {
-                    message = stdout;
-                }
-
-                if (code !== 0) {
-                    message = message.trim();
-                    const formatted = `Could not format, code: ${code}, error: ${message}`;
-                    reject(formatted);
+                if (stderrChunks.length > 0 || code !== 0) {
+                    const message = stderrChunks.join("").concat(stdoutChunks.join("")).trim();
+                    reject(`Could not format, code: ${code}, error: ${message}`);
                 } else {
-                    resolve([new TextEdit(range, stdout)]);
+                    resolve([new TextEdit(range, stdoutChunks.join(""))]) ;
                 }
             });
         }).catch((reason) => {
