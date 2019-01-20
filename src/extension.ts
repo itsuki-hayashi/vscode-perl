aaac04e246a1c48232bc8806fe9639c259245dac
edsgerlin <edsgerlin@icloud.com>
2019-01-20 21:54:11 +0900

    Add formatter to extension.ts.

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-
+import { PerlFormattingProvider } from "./format";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    // tslint:disable-next-line:no-console
     console.log('Congratulations, your extension "simple-perl" is now active!');
 
     // The command has been defined in the package.json file
@@ -22,6 +21,13 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(disposable);
+
+    const formatProvider = new PerlFormattingProvider();
+    context.subscriptions.push(
+        vscode.languages.registerDocumentRangeFormattingEditProvider(
+            { language: "perl", scheme: "file" },
+            formatProvider));
+
 }
 
 // this method is called when your extension is deactivated
